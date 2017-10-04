b7e1169a0b5e08da1358a745ccec0c4735e0fce8
Brian <itsbriantruong@gmail.com>
2017-10-03 23:00:04 -0700

    Basic app with dependencies setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './css/index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+import configureStore from "./store/configureStore";
+const store = configureStore();
+
+const rootEl = document.getElementById("root");
+
+// Create a reusable render method that we can call more than once
+let render = () => {
+    const App = require("./App").default;
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootEl
+    );
+};
+
+if (module.hot) {
+    // Support hot reloading of components
+    // and display an overlay for runtime errors
+    const renderApp = render;
+    const renderError = (error) => {
+        const RedBox = require("redbox-react").default;
+        ReactDOM.render(
+            <RedBox error={error} />,
+            rootEl,
+        );
+    };
+
+    // In development, we wrap the rendering function to catch errors,
+    // and if something breaks, log the error and render it to the screen
+    render = () => {
+        try {
+            renderApp();
+        }
+        catch(error) {
+            console.error(error);
+            renderError(error);
+        }
+    };
+
+    // Whenever the App component file or one of its dependencies
+    // is changed, re-import the updated component and re-render it
+    module.hot.accept("./App", () => {
+        setTimeout(render);
+    });
+}
+
+render();
 registerServiceWorker();
---
+++ src/reducers/testReducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+    data : 42332
+};
+
+export default function testReducer(state = initialState, action) {
+    return state;
+}
